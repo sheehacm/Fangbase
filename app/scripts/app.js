8b37139dceaeffdfb7cf9998298050a52be7389d
sheehacm <sheehacm@dukes.jmu.edu>
2015-07-20 22:17:29 -0400

    added basic array adding and storing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,34 @@
-'use strict';
+/* global app:true */
+/* exported app */
 
-/**
- * @ngdoc overview
- * @name fangbaseApp
- * @description
- * # fangbaseApp
- *
- * Main module of the application.
- */
-angular
-  .module('fangbaseApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name fangbaseApp
+   * @description
+   * # fangbaseApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('fangbaseApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/posts.html',
+          controller: 'PostsCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
+})();
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+
+	angular.module('fangbaseApp')
+		.controller('PostsCtrl', function ($scope) {
+			$scope.posts = [];
+			$scope.post = {url: 'http://', title: ''};
+
+			$scope.submitPost = function () {
+		      $scope.posts.push($scope.post);
+		      $scope.post = {url: 'http://', title: ''};
+		    };
+
+		    $scope.deletePost = function (index) {
+			  $scope.posts.splice(index, 1);
+			};
+		});    
+})();
